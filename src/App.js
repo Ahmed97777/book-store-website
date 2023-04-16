97f496bf11a3330a264427aa8048d1973d9842dd
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-04-17 01:30:53 +0200

    continued setting up.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBook , faCartShopping, faUser} from '@fortawesome/free-solid-svg-icons';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faBook , faCartShopping, faUser, faAddressBook} from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HeaderNavbar from "./Components/HeaderNavbar";
+import NotFound from "./Components/NotFound"
 
 function App() {
 
-  library.add(faBook);
+  // library.add(faBook);
 
   return (
 
     <div className="App">
 
-      <h1>Welcome to our website</h1>
+      
+
+
+      <>
+      
+        <BrowserRouter>
+          <Routes>
+
+            <Route  path="/" element= {<div><HeaderNavbar/></div>} />
+
+            <Route  path="*" element= {<div><HeaderNavbar/><NotFound/></div>} />
+
+          </Routes>
+        </BrowserRouter>
+      
+      
+      </>
+
+
+      {/* <h1>Welcome to our website</h1>
 
       <div className="alert alert-primary" role="alert">
         A simple primary alert—check it out!
@@ -23,6 +50,8 @@ function App() {
       <FontAwesomeIcon icon={faCartShopping} />
 
       <FontAwesomeIcon icon={faUser} />
+
+      <FontAwesomeIcon icon={faAddressBook} /> */}
 
     </div>
   );
